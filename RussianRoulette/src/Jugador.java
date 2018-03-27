ea9fa2650687ef78e74b5b60d6799aa578ca1632
Darkenend <alvaroreal199@gmail.com>
2018-03-27 15:16:39 +0200

    Started Russian Roulette game

--- RussianRoulette/src/Jugador.java
+++ RussianRoulette/src/Jugador.java
@@ -1,4 +1,8 @@
+import java.util.Random;
+
 /**
+ * <h1>Jugador</h1>
+ *
  * @author Álvaro Real
  * @author darkenend.net
  * @version 1.0.0
@@ -6,11 +10,71 @@
 
 public class Jugador {
     /*
-        TODO Create attribute "id"
-        TODO Create attribute "nombre"
-        TODO Create attribute "vivo"
-        TODO Method "disparar(Revolver r)"
-        TODO Add documentation of attributes
         TODO Add documentation of methods
      */
+    private int id;
+    private String nombre;
+    private boolean vivo;
+
+    /**
+     * Jugador constructor, which basically needs the two parameters to be completely created,
+     * "vivo" is always true by default, meaning that the player is alive.
+     * @param i_id The id will come from the outside, but not input by the players.
+     * @param i_nombre The name will come from the outside, and will be player input.
+     */
+    public Jugador(int i_id, String i_nombre) {
+        System.out.println("Loading player...");
+        this.id=i_id;
+        this.nombre=i_nombre;
+        this.vivo=true;
+        System.out.println("Player "+this.nombre+" with ID "+this.id+" has been created.");
+    }
+
+    public boolean disparar(Revolver r) {
+        Random rng = new Random();
+        int rngeezuswills = rng.nextInt(4);
+        if (r.disparar()) {
+            if (this.nombre=="Darkenend") {
+                System.out.println("BUENO CARALLO");
+            } else {
+                System.out.println("You died.");
+            }
+            switch (rngeezuswills) {
+                case 0:
+                    System.out.println("And we see a bit of pink mist, and everyone wipes their faces, just in case.");
+                    break;
+                case 1:
+                    System.out.println("BANG, whoops, "+this.nombre+"is dead.");
+                    break;
+                case 2:
+                    System.out.println("Press F to pay respects.\nWait wrong game.");
+                    break;
+                case 3:
+                    System.out.println("BOOM, HEADSHOT");
+                    break;
+            }
+            this.vivo=false;
+            return false;
+        }
+        else {
+            if (this.nombre=="Darkenend") {
+                System.out.println("VAS MORRER");
+            }
+            switch (rngeezuswills) {
+                case 0:
+                    System.out.println("CLICK, you ain't dead yet!");
+                    break;
+                case 1:
+                    System.out.println(this.nombre+"is one lucky pal.");
+                    break;
+                case 2:
+                    System.out.println("HEY, YOU'RE NOT DEAD!");
+                    break;
+                case 3:
+                    System.out.println("You still got it under control, right?");
+                    break;
+            }
+            return true;
+        }
+    }
 }
--- RussianRoulette/src/Revolver.java
+++ RussianRoulette/src/Revolver.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 
 /**
+ * <h1>Revolver</h1>
+ *
  * @author Álvaro Real
  * @author darkenend.net
  * @version 1.0.0
@@ -8,20 +10,20 @@ import java.util.Random;
 
 public class Revolver {
     /*
-        TODO Method "siguienteBala()", which changes position in the drum
         TODO Add documentation of each method
-     */
-    /**
-     * This will give info about each class attribute
-     * @param current_position This indicates which round slot is being shot off the revolver.
-     * @param round_position This indicates in which slot the round is.
-     * @param capacity This indicates the maximum capacity of the revolver
      */
     private int current_position;
     private int round_position;
     private int capacity;
 
-
+    /**
+     * Main constructor for the Revolver, it will randomly generate a
+     * revolver of a capacity ranging from 5 to 8 rounds, and then it
+     * will load it. This means that it generates an int with a maximum,
+     * and then assigns a random value to the current_position
+     * reffering to the slot in the revolver's drum where the round
+     * is.
+     */
     public Revolver() {
         Random rng = new Random();
         this.capacity= rng.nextInt(3)+5;
@@ -31,14 +33,25 @@ public class Revolver {
         System.out.println("The revolver has been loaded. It has 1 round out of "+this.capacity+".");
     }
 
+    /**
+     * This method will compare where the hammer is aiming (current_position)
+     * to the slot where the round is (round_position)
+     * @return true if current_position==round_position (which would mean that
+     * the revolver has shot a bullet and killed someone.)
+     */
     public boolean disparar() {
         System.out.println("Aims at his head and...");
         if (this.current_position==this.round_position) return true;
         else return false;
     }
 
+    /**
+     * This method will change the slot where the hammer is aiming (current_position)
+     * to another one. There's two modes available, and the dev should un-comment and
+     * re-comment code for it to switch between the "Roll between shots" and the
+     * "Sequential shooting mode."
+     */
     public void siguienteBala() {
-        //Swap comments between the two blocks to switch between the "Roll between shots" and "Sequential shooting"
         //Roll between shots
         //Random rng = new Random();
         //System.out.println("Let's give it a spin...");
@@ -49,6 +62,11 @@ public class Revolver {
         else this.current_position=this.current_position+1;
     }
 
+    /**
+     * Method that takes all the information of the revolver into a string.
+     * @return String that contains all the information about the revolver,
+     * basically the current_position and the round_position of said revolver.
+     */
     public String toString() {
         return "The revolver is gonna shoot the slot "+this.current_position+" and the bullet is in slot "+this.round_position+".";
     }
