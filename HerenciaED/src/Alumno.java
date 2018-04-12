637bc5b52db05bb668a7617171a01a2f68d08f29
Darkenend <alvaroreal199@gmail.com>
2018-04-12 20:54:31 +0200

    Started Inheritance exercises for Development Environments.

---
+++ HerenciaED/src/Alumno.java
@@ -0,0 +1,15 @@
+/**
+ * @author Álvaro Real
+ * @author darkenend.net
+ * @version 1.0.0
+ */
+
+public class Alumno {
+    /*
+        TODO Implement attributes
+        TODO Implement methods
+        TODO Figure more stuff
+     */
+    protected String name, house, fav_sub = "Entornos de Desarrollo mágicos", reply;
+
+}
---
+++ HerenciaED/src/Gryffindor.java
@@ -0,0 +1,9 @@
+/**
+ * @author Álvaro Real
+ * @author darkenend.net
+ * @version 1.0.0
+ */
+
+public class Gryffindor extends Alumno {
+
+}
