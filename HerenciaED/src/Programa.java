f6adfe4b2e2239fa04e0f13846ded2002aaf29f5
Darkenend <alvaroreal199@gmail.com>
2018-04-24 15:47:45 +0200

    After the previous foundations were planted, a big update has been pushed, changes *a lot* of stuff, so basically this is the program on 2.0.0

--- HerenciaED/src/Programa.java
+++ HerenciaED/src/Programa.java
@@ -6,50 +6,67 @@ import java.util.Scanner;
  * it's set in the Harry Potter Universe
  * @author Álvaro Real
  * @author darkenend.net
- * @version 1.2.0
+ * @version 2.0.0
  */
 
 public class Programa {
     private static int[] student_amount = new int[4];
+    private static Alumno[] op;
+    private static boolean devmode = false;
+    private static int counter = 0, current_house = 0;
 
     /**
      * @param args Command line arguments
      */
     public static void main(String[] args) {
         studentInput();
-        Alumno[] op = new Alumno[student_amount[0]+student_amount[1]+student_amount[2]+student_amount[3]];
-        studentCreate(op, student_amount[0]);
-        studentCreate(op, student_amount[1]);
-        studentCreate(op, student_amount[2]);
-        studentCreate(op, student_amount[3]);
-        studentPrint(op);
+        op = new Alumno[student_amount[0]+student_amount[1]+student_amount[2]+student_amount[3]];
+        if (devmode == true) System.err.println(op.length);
+        studentCreate(student_amount[0]);
+        studentCreate(student_amount[1]);
+        studentCreate(student_amount[2]);
+        studentCreate(student_amount[3]);
+        studentPrint();
         printHouseCount(student_amount);
     }
 
     /**
      * This method uses a loop to create different students
-     * @param alumni_array This is an array of the "Alumno" object that stores the alumni's information
      * @param house_count This is a int array that basically keeps track of how many alumni have been created
      */
-    public static void studentCreate(Alumno[] alumni_array, int house_count) {
+    public static void studentCreate(int house_count) {
         for (int i = 0; i < house_count; i++) {
-
+            switch (current_house) {
+                case 0:
+                    op[counter]=new Gryffindor();
+                    break;
+                case 1:
+                    op[counter]=new Slytherin();
+                    break;
+                case 2:
+                    op[counter]=new Ravenclaw();
+                    break;
+                case 3:
+                    op[counter]=new Hufflepuff();
+                    break;
+            }
+            counter++;
         }
+        current_house++;
     }
 
     /**
      * This method uses a loop to print the students' information
-     * @param alumni_array This is an array of the "Alumno" object that stores the alumni's information
      */
-    public static void studentPrint(Alumno[] alumni_array) {
-        for (int i = 0; i < alumni_array.length; i++) {
+    public static void studentPrint() {
+        for (int i = 0; i < op.length; i++) {
             System.out.println("Alumno "+(i+1));
-            System.out.println("Nombre: "+alumni_array[i].getFullName());
-            System.out.println("Casa: "+alumni_array[i].getHouse());
-            System.out.println("Edad: "+alumni_array[i].getAge());
-            System.out.println("Opinion sobre los deberes: "+alumni_array[i].getReply());
-            System.out.println("Asignatura preferida: "+alumni_array[i].getFav_sub());
-            System.out.println(alumni_array[i].getProperty());
+            System.out.println("Nombre: "+op[i].getFullName());
+            System.out.println("Casa: "+op[i].getHouse());
+            System.out.println("Edad: "+op[i].getAge());
+            System.out.println("Opinion sobre los deberes: "+op[i].getReply());
+            System.out.println("Asignatura preferida: "+op[i].getFav_sub());
+            System.out.println(op[i].getProperty());
             System.out.println();
         }
     }
@@ -63,29 +80,41 @@ public class Programa {
 
     public static void studentInput() {
         Scanner kb = new Scanner(System.in);
-        System.out.println("Introduce el numero de alumnos de Gryffindor: ");
-        student_amount[0]=kb.nextInt();
-        while (student_amount[0]<1) {
-            System.out.println("Introduce un numero positivo");
-            student_amount[0]=kb.nextInt();
+        for (int i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("Introduce el numero de alumnos de Gryffindor: ");
+                    break;
+                case 1:
+                    System.out.println("Introduce el numero de alumnos de Slytherin: ");
+                    break;
+                case 2:
+                    System.out.println("Introduce el numero de alumnos de Ravenclaw: ");
+                    break;
+                case 3:
+                    System.out.println("Introduce el numero de alumnos de Hufflepuff: ");
+            }
+            student_amount[i]=kb.nextInt();
+            student_amount[i]=setDevmode(student_amount[i]);
+            while (student_amount[i]<1) {
+                System.out.println("Introduce un numero positivo");
+                student_amount[i]=kb.nextInt();
+            }
         }
-        System.out.println("Introduce el numero de alumnos de Slytherin: ");
-        student_amount[1]=kb.nextInt();
-        while (student_amount[1]<1) {
-            System.out.println("Introduce un numero positivo");
-            student_amount[1]=kb.nextInt();
-        }
-        System.out.println("Introduce el numero de alumnos de Ravenclaw: ");
-        student_amount[2]=kb.nextInt();
-        while (student_amount[2]<1) {
-            System.out.println("Introduce un numero positivo");
-            student_amount[2]=kb.nextInt();
-        }
-        System.out.println("Introduce el numero de alumnos de Hufflepuff: ");
-        student_amount[3]=kb.nextInt();
-        while (student_amount[3]<1) {
-            System.out.println("Introduce un numero positivo");
-            student_amount[3]=kb.nextInt();
+    }
+
+    /**
+     * This method takes an integer to validate if the Developer Mode is active
+     * @param xd The integer that it's fed to the method to validate the activation of the dev mode
+     * @return Returns -1 if devmode is active to re-input a number, the standard number if it isn't 273
+     */
+    public static int setDevmode(int xd) {
+        if (xd == 273) {
+            devmode = true;
+            System.err.println("DEVMODE ACTIVATED");
+            return -1;
+        } else {
+            return xd;
         }
     }
 }
